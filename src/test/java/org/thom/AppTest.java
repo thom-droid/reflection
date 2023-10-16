bd56097c9931e23134ce6d4ec3c085646dc75e07
thom-window <hsbang.thom@gmail.com>
2023-10-17 00:11:39 +0900

    annotations

--- src/test/java/org/thom/AppTest.java
+++ src/test/java/org/thom/AppTest.java
@@ -2,19 +2,32 @@ package org.thom;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 public class AppTest {
 
     @Test
-    public void annotationTest() {
+    public void mybookAnnotations() {
 
-        Class<Book> bookClass = Book.class;
-        Annotation[] annotations = bookClass.getAnnotations();
-        System.out.println("annotations = " + Arrays.toString(annotations));
+        // check if annotation is inherited
+
+        // annotation scoped to MyBook
+        Annotation[] annotations = MyBook.class.getAnnotations();
+
+        Arrays.stream(annotations).forEach(a -> Assert.assertTrue(a.annotationType().isAssignableFrom(BookAnnotation.class)));
+
+        String value = "une belle ami";
+        int num = 2;
+
+        BookAnnotation bookAnnotation = MyBook.class.getAnnotation(BookAnnotation.class);
+
+        Assert.assertEquals(value, bookAnnotation.value());
+        Assert.assertEquals(num, bookAnnotation.number());
 
     }
 
